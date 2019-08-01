7f9f8f1f9c93c58eeacf444dc212ad6b059d803b
porterwilcox <wilcox.porter@gmail.com>
2019-07-31 20:52:24 -0600

    JJ

--- Program.cs
+++ Program.cs
@@ -14,26 +14,36 @@ namespace NumberGuess
             // If number matches - game stops
             // Returns message for another user input
             // User provides input again if the number doesn't match!
-             Random rndm = new Random();
-             int compNum = rndm.Next(1,101);
+            Random rndm = new Random();
+            int compNum = rndm.Next(1, 101);
+            while (true)
+            {
+                Console.WriteLine("Please guess a number between 1 and 100");
+                string userInput = Console.ReadLine();
+                int guess = 0;
+                bool isInt = Int32.TryParse(userInput, out guess);
+                if (!isInt)
+                {
+                    Console.WriteLine("That isn't a number");
 
-             Console.WriteLine("Please guess a number between 1 and 100");
-             string userInput = Console.ReadLine();
-             int guess = 0;
-               bool isInt = Int32.TryParse(userInput, out guess);
-               if (!isInt)
-               {
-                   Console.WriteLine("That isn't a number");
-                
-               }
-             if (guess < 1 || guess > 100)
-             {
-                 Console.WriteLine("Select a number between 1 and 100");
-             }
-
-
-        
-
+                }
+                if (guess < 1 || guess > 100)
+                {
+                    Console.WriteLine("Select a number between 1 and 100");
+                }
+                if (compNum > guess)
+                {
+                    Console.WriteLine("Guess lower.");
+                }
+                else if (compNum < guess)
+                {
+                    Console.WriteLine("Guess higher.");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed right!");
+                }
+            }
         }
     }
 }
