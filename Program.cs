c9b1da2eeb55f7dc4d828d7a574c607c9cfe74c7
porterwilcox <wilcox.porter@gmail.com>
2019-07-31 20:39:16 -0600

    Michael

--- Program.cs
+++ Program.cs
@@ -16,7 +16,13 @@ namespace NumberGuess
             // User provides input again if the number doesn't match!
              Random rndm = new Random();
              int compNum = rndm.Next(1,101);
+
+             Console.WriteLine("Please guess a number between 1 and 100");
+             string userInput = Console.ReadLine();
+             int guess = 0;
+             Int32.TryParse(userInput, out guess);
              
+
 
         
 
